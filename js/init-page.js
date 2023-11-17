33f060c5f39766e670769addb3956fd699c0b1f1
lizard512 <lizard@smail.nchu.edu.tw>
2023-11-17 23:27:08 +0800

    Last of day

--- js/init-page.js
+++ js/init-page.js
@@ -1,16 +1,20 @@
-window.onload = async function () {
-    let response = await fetch('navbar.html');
-    let data = await response.text();
-    document.getElementById('navbar').innerHTML = data;
+const nav = `
+<ul>
+    <li><a href="HW01.html">HW01</a></li>
+    <li><a href="HW02.html">HW02</a></li>
+    <li><a href="HW03.html">HW03</a></li>
+    <li><a href="#">HW04</a></li>
+    <li><a href="#">HW05</a></li>
+    <li><a href="#">HWEX</a></li>
+</ul>
+`
+document.getElementById("navbar").insertAdjacentHTML('afterbegin', nav);
 
-    let currentPage = window.location.pathname.split('/').pop();
-    let links = document.querySelectorAll('#navbar a');
-    for (let link of links) {
-        if (link.getAttribute('href') === currentPage) {
-            link.classList.add('current-page');
-            break;
-        }
+let currentPage = window.location.pathname.split('/').pop();
+let links = document.querySelectorAll('#navbar a');
+for (let link of links) {
+    if (link.getAttribute('href') === currentPage) {
+        link.classList.add('current-page');
+        break;
     }
-
-    document.body.style.visibility = 'visible';
-};
+}
