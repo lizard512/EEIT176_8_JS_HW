103ad4a884ebea3bf2b7fc65a8ef357ca95d6c43
LiTseLin <lizard@smail.nchu.edu.tw>
2023-11-20 07:19:27 +0800

    S1

--- js/init-page.js
+++ js/init-page.js
@@ -1,11 +1,11 @@
 let nav = `
 <ul>
-    <li><a href="HW01.html">HW01</a></li>
-    <li><a href="HW02.html">HW02</a></li>
-    <li><a href="HW03.html">HW03</a></li>
-    <li><a href="#">HW04</a></li>
-    <li><a href="#">HW05</a></li>
-    <li><a href="#">HWEX</a></li>
+    <li><a href="HW01.html"><span class ="fa fa-calculator"></span> HW01</a></li>
+    <li><a href="HW02.html"><span class ="fa fa-file-text"></span>HW02</a></li>
+    <li><a href="HW03.html"><span class ="fa fa-star-half-o">HW03</a></li>
+    <li><a href="HW04.html"><span class ="fa fa-file-text"></span> HW04</a></li>
+    <li><a href="HW05.html"><span class ="fa fa-file-text">HW05</a></li>
+    <li><a href="HWEX.html"><span class ="fa fa-file-text">HWEX</a></li>
 </ul>
 `
 document.getElementById("navbar").insertAdjacentHTML('afterbegin', nav);
